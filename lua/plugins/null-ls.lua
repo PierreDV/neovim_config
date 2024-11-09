ec07e922a0ec42cec301e8c05c17c406d2705014
PierreDV <p.verteuil@gmail.com>
2024-11-09 13:29:45 +0100

    fix eslint package error

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,5 +1,8 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
@@ -10,7 +13,7 @@ return {
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.rubocop,
         null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.diagnostics.eslint_d,
+        require("none-ls.diagnostics.eslint_d"),
       },
     })
 
