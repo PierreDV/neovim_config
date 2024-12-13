96d306b2092a9b27c9c721824247340f730375f8
PierreDV <p.verteuil@gmail.com>
2024-12-13 12:37:11 +0100

    wrap confis in sources object

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,7 +4,7 @@ return {
     local null_ls = require("null-ls")
 
     null_ls.setup({
-      null_ls.builtins.formatting.stylua
+      sources = { null_ls.builtins.formatting.stylua }
     })
     vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
   end
