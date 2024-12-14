aa7cbba3b7b5ee6b7c93014051145a0465015ec4
PierreDV <p.verteuil@gmail.com>
2024-12-14 16:41:15 +0100

    setup autocomplete

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,12 +1,13 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local lspconfig = require("lspconfig")
-		lspconfig.ts_ls.setup({})
-		lspconfig.lua_ls.setup({})
+  "neovim/nvim-lspconfig",
+  config = function()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local lspconfig = require("lspconfig")
+    lspconfig.ts_ls.setup({ capabilities = capabilities })
+    lspconfig.lua_ls.setup({ capabilities = capabilities })
 
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-	end,
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+  end,
 }
