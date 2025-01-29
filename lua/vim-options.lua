e399cc3e1255e64506c5c424013180debd262008
Peter de Verteuil <peter.dv@treecompany.be>
2025-01-29 10:51:03 +0100

    remap page up and down commands

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,3 +6,5 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 vim.g.mapleader = " "
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
