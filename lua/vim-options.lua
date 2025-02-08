9a3747da04576795226511a9e5e3d4a24e0298c6
Peter de Verteuil <peter.dv@treecompany.be>
2025-02-08 11:18:01 +0100

    add more keybindings

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,5 +6,18 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 vim.g.mapleader = " "
+vim.g.terminal_emulator = 'ghostty'
+
+-- horizontal movement
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- moving between panes
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<CR>")
+vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<CR>")
+vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<CR>")
+vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<CR>")
