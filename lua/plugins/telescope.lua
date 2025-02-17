56f345ddff97608e02ebcdc3c1ad5c896a437287
Peter de Verteuil <peter.dv@treecompany.be>
2025-02-17 10:00:17 +0100

    enable greppign of hidden files and show relative path in status bar

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,13 +7,12 @@ return {
     --vim.keymap.set("n", "<D-p>", builtin.find_files, {})
     vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    require('telescope').setup {
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+    require("telescope").setup({
       pickers = {
-        buffers = {
-          initial_mode = "normal",
-        }
-      }
-    }
+        buffers = { initial_mode = "normal" },
+        find_files = { hidden = true },
+      },
+    })
   end,
 }
