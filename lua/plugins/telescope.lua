2cf47e367311309a8687df4af83831007cb0bd20
Peter de Verteuil <peter.dv@treecompany.be>
2025-03-01 20:49:24 +0100

    update options

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,17 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local builtin = require("telescope.builtin")
-    --vim.keymap.set("n", "<D-p>", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-    require("telescope").setup({
-      pickers = {
-        buffers = { initial_mode = "normal" },
-        find_files = { hidden = true },
-      },
-    })
-  end,
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
+
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		require("telescope").setup({
+			pickers = {
+				buffers = { initial_mode = "normal" },
+			},
+		})
+	end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,14 +6,14 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 vim.g.mapleader = " "
--- vim.api.nvim_set_option("clipboard", "unnamed")
+vim.api.nvim_set_option("clipboard", "unnamed")
 
 -- horizontal movement
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- moving between panes
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
