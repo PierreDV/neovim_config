a7be1f3eb05764f8651a8d05ea9da50ce95b8d1e
Peter de Verteuil <peter.dv@treecompany.be>
2025-05-18 10:23:09 +0200

    ensure things are sintalled

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,8 +1,8 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
-  config = function()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "ts_ls", "lua_ls" },
-    })
-  end,
+	"williamboman/mason-lspconfig.nvim",
+	config = function()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "ruby_lsp", "ts_ls", "lua_ls" },
+		})
+	end,
 }
