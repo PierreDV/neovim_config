d4dd8ce097c7fe00430338ef36372bed1b7a99d3
PierreDV <peter.verteuil@proton.me>
2026-02-12 11:18:15 +0100

    update lsp icons

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -50,10 +50,10 @@ local severity = vim.diagnostic.severity
 vim.diagnostic.config({
 	signs = {
 		text = {
-			[severity.ERROR] = "󰈸 ",
-			[severity.WARN] = "󰊠 ",
-			[severity.HINT] = " ",
-			[severity.INFO] = " ",
+			[severity.ERROR] = "󰈸",
+			[severity.WARN] = "󰊠",
+			[severity.HINT] = "󱩎",
+			[severity.INFO] = "",
 		},
 	},
 })
